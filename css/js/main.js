efe577acf93ad378aa51ab8b69d6348757430b92
Simão Ildefonso <simaoildefonso@ua.pt>
2023-05-24 10:17:33 +0100

    bota na pipikinha

--- css/js/main.js
+++ css/js/main.js
@@ -13,3 +13,20 @@ function checkInp()
         return false;
     }
 }
+
+function mostrarInput() {
+    
+    var Mbway= document.querySelector("#MBWay");
+    var numtel= document.querySelector("#numtel");
+
+    if (Mbway.checked==true) {
+        numtel.style.display = "block";
+        numtel.style.animation = "Mbway 1s ease-in-out infinite";
+        numtel.style.background = "#f2f2f2";
+        numtel.style.border = "1px solid #ccc";
+    } else {
+        numtel.style.display = "none";
+    }
+}
+   
+    
